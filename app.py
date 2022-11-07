57239e012b6bc33b250a71da39c62f9840e56ce6
rainerioagbayani <agbayanieri21@gmail.com>
2022-11-07 12:36:44 +0800

    initial commit

---
+++ app.py
@@ -0,0 +1,156 @@
+# libraries
+import random
+import numpy as np
+import pickle
+import json
+from flask import Flask, render_template, request, url_for, flash, redirect, session, logging
+from flask_sqlalchemy import SQLAlchemy
+from flask_ngrok import run_with_ngrok
+import nltk
+from keras.models import load_model
+from nltk.stem import WordNetLemmatizer
+lemmatizer = WordNetLemmatizer()
+
+
+# chat initialization
+model = load_model("chatbot_model.h5")
+intents = json.loads(open("intents.json").read())
+words = pickle.load(open("words.pkl", "rb"))
+classes = pickle.load(open("classes.pkl", "rb"))
+
+app = Flask(__name__)
+app.secret_key = "super secret key"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////Users/ADMIN PC/Desktop/softeng\An-AI-Chatbot-in-Python-and-Flask/database.db'
+db = SQLAlchemy(app)
+# run_with_ngrok(app) 
+
+class user(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80))
+    email = db.Column(db.String(120))
+    password = db.Column(db.String(80))
+# login
+@app.route("/",methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        uname = request.form["uname"]
+        passw = request.form["passw"]
+        
+        login = user.query.filter_by(username=uname, password=passw).first()
+        if login is not None:
+            return redirect(url_for("home"))
+    return render_template("login.html")
+# register
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        uname = request.form['uname']
+        mail = request.form['mail']
+        passw = request.form['passw']
+
+        register = user(username = uname, email = mail, password = passw)
+        db.session.add(register)
+        db.session.commit()
+
+        return redirect(url_for("login"))
+    return render_template("register.html")
+
+# logout 
+@app.route('/logout')
+def logout():
+    session.pop('user_id', None)
+    flash('You have successfully logged out.')
+    return redirect(url_for("login"))
+# homepage
+@app.route("/home")
+def home():
+    return render_template("home.html")
+
+@app.route("/chatbot")
+def chat_bot():
+    return render_template("chatbot.html")
+
+@app.route("/hotlines")
+def hotlines():
+    return render_template("hotlines.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/organizations")
+def org():
+    return render_template("organizations.html")
+
+@app.route("/support")
+def support():
+    return render_template("support.html")
+
+@app.route("/get", methods=["POST"])
+def chatbot_response():
+    msg = request.form["msg"]
+    if msg.startswith('my name is'):
+        name = msg[11:]
+        ints = predict_class(msg, model)
+        res1 = getResponse(ints, intents)
+        res =res1.replace("{n}",name)
+    elif msg.startswith('hi my name is'):
+        name = msg[14:]
+        ints = predict_class(msg, model)
+        res1 = getResponse(ints, intents)
+        res =res1.replace("{n}",name)
+    else:
+        ints = predict_class(msg, model)
+        res = getResponse(ints, intents)
+    return res
+
+
+# chat functionalities
+def clean_up_sentence(sentence):
+    sentence_words = nltk.word_tokenize(sentence)
+    sentence_words = [lemmatizer.lemmatize(word.lower()) for word in sentence_words]
+    return sentence_words
+
+
+# return bag of words array: 0 or 1 for each word in the bag that exists in the sentence
+def bow(sentence, words, show_details=True):
+    # tokenize the pattern
+    sentence_words = clean_up_sentence(sentence)
+    # bag of words - matrix of N words, vocabulary matrix
+    bag = [0] * len(words)
+    for s in sentence_words:
+        for i, w in enumerate(words):
+            if w == s:
+                # assign 1 if current word is in the vocabulary position
+                bag[i] = 1
+                if show_details:
+                    print("found in bag: %s" % w)
+    return np.array(bag)
+
+
+def predict_class(sentence, model):
+    # filter out predictions below a threshold
+    p = bow(sentence, words, show_details=False)
+    res = model.predict(np.array([p]))[0]
+    ERROR_THRESHOLD = 0.25
+    results = [[i, r] for i, r in enumerate(res) if r > ERROR_THRESHOLD]
+    # sort by strength of probability
+    results.sort(key=lambda x: x[1], reverse=True)
+    return_list = []
+    for r in results:
+        return_list.append({"intent": classes[r[0]], "probability": str(r[1])})
+    return return_list
+
+
+def getResponse(ints, intents_json):
+    tag = ints[0]["intent"]
+    list_of_intents = intents_json["intents"]
+    for i in list_of_intents:
+        if i["tag"] == tag:
+            result = random.choice(i["responses"])
+            break
+    return result
+
+
+if __name__ == "__main__":
+    app.run()
